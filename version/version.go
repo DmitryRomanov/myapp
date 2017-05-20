2640702069827ff065b53dd446aee0a7da68deab
Dmitry Romanov <dmitry.romanov85@gmail.com>
2017-05-20 18:06:56 +0700

    release 0.1.1

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// RELEASE returns the release version
-	RELEASE = "UNKNOWN"
+	RELEASE = "0.1.1"
 	// REPO returns the git repository URL
 	REPO = "UNKNOWN"
 	// COMMIT returns the short sha from git
